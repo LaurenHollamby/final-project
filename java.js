ed1f3b56159918851efb8b34941375ef96f2e45e
LaurenHollamby <lauren.hollamby@edu.sait.ca>
2025-04-18 12:53:10 -0600

    slideshow

--- java.js
+++ java.js
@@ -19,3 +19,23 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=eebafbbdb2
     
     
     })
+
+    let slideIndex = 1;
+    showSlides (slideIndex);
+
+    function plusSlides (n) {
+        showSlides (slideIndex += n);
+    }
+
+    function showSlides (n) {
+        let i;
+        let slides = document.getElementsByClassName ("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000)
+}
+    
